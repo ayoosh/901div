4fdb20b28c9a13419db16e0d0fb53e9dbac902be
Ayoosh Bansal <ayoosh4@gmail.com>
2014-02-11 17:16:26 -0600

    yay we know what to do in proj!!

--- top_level.v
+++ top_level.v
@@ -16,10 +16,5 @@ always@(posedge clk, posedge RST) begin
 end
 
 // main body
-i2c_master_top i2c(
-	wb_clk_i, wb_rst_i, arst_i, wb_adr_i, wb_dat_i, wb_dat_o,
-	wb_we_i, wb_stb_i, wb_cyc_i, wb_ack_o, wb_inta_o,
-	scl_pad_i, scl_pad_o, scl_padoen_o, sda_pad_i, sda_pad_o, sda_padoen_o );
-
 
 endmodule
